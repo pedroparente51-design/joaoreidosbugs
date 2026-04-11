d1fc66eb39c9f3715523b0624dcb51770422aad5
Pedro <pedroparente51@gmail.com>
2026-04-11 17:37:44 -0300

    chore: clean up test scripts

--- frontend/scratch.js
+++
@@ -1,4 +0,0 @@
-const axios = require('axios');
-const api = axios.create({ baseURL: '/api' });
-console.log(api.getUri({ url: '/notifications/test' }));
-console.log(api.getUri({ url: 'notifications/test' }));
